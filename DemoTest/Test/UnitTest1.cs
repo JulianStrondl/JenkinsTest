83d5cd993099a7b8b7617ceae3219f8f7a17956d
Julian Strondl <j.strondl@htlkrems.at>
2021-10-27 15:50:54 +0200

    yeeTest

--- DemoTest/Test/UnitTest1.cs
+++ DemoTest/Test/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace Test
     [TestClass]
     public class UnitTest1
     {
-        private const string Expected = "Hello World!";
+        private const string Expected = "yee!";
         [TestMethod]
         public void TestMethod1()
         {
